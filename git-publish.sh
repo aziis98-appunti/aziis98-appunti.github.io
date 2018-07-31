8fa4fc2e4cc833b8d8e13a93273c6a448b3b3bc9
aziis98 <antonio.delucreziis@gmail.com>
2018-08-01 01:53:24 +0200

    [A[BMaybe fixed publish script

--- git-publish.sh
+++ git-publish.sh
@@ -1,4 +1,8 @@
-npm run build
-git add .
-git commit -am "Built project"
+#!/bin/sh
+
+read -p "Commit description: " desc
+
+npm run build && \
+git add . && \
+git commit -m "$desc" && \
 git push -f origin `git subtree split -P public`:master
